5fbeddb1d488c9de855824bb394fabc4b455d78e
Carlos Argueta <kid.a.rgueta@gmail.com>
2025-03-18 19:19:09 +0800

    update: added lesson 5 code

---
+++ lesson5/rse_fsd/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'rse_fsd'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='carlos',
+    maintainer_email='kid.a.rgueta@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'command_node = rse_fsd.command_node:main',
+            'camera_node = rse_fsd.camera_node:main',
+            'class_counts_client = rse_fsd.class_counts_client:main',
+        ],
+    },
+)
