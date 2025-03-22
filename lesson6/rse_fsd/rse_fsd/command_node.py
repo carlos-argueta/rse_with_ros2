3844b3f01c0b7224e86fd5681d53a8fabdf18610
Carlos Argueta <kid.a.rgueta@gmail.com>
2025-03-23 07:30:15 +0800

    update: added lesson 6 code

--- lesson6/rse_fsd/rse_fsd/command_node.py
+++ lesson6/rse_fsd/rse_fsd/command_node.py
@@ -4,29 +4,28 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class ControlCenterNode(Node):
+class CommandNode(Node):
 
     def __init__(self):
-        super().__init__('control_center_node')
+        super().__init__('command_node')
         self.publisher_ = self.create_publisher(String, 'command', 10)
         self.get_logger().info('Command publisher started')
-       
+        
     def publish_command(self, command):
         msg = String()
         msg.data = command
         self.publisher_.publish(msg)
         self.get_logger().info(f'Published command: "{msg.data}"')
 
-
 def main(args=None):
     rclpy.init(args=args)
 
-    control_center_node = ControlCenterNode()
+    command_node = CommandNode()
 
     while rclpy.ok():
-        command = input("Local change Enter command (r=rgb / b=bgr / g=gray / f=flip / q=quit): ") # Get commands from the user
+        command = input("Enter command (r=rgb / b=bgr / g=gray / f=flip / q=quit): ") # Get commands from the user
         if command in ['r', 'b', 'g', 'f', 'q']:
-            control_center_node.publish_command(command)
+            command_node.publish_command(command)
         else:
             print("Invalid command. Use r, g, f, b or q.")
 
@@ -36,7 +35,7 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    control_center_node.destroy_node()
+    command_node.destroy_node()
     rclpy.shutdown()
 
 
